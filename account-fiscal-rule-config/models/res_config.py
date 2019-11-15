a8efb4e217d84503835dd626fa038ce6ab401b7e
Maher <maher@tenovar.com>
2019-11-15 10:10:39 +0100

    update res_config

---
+++ account-fiscal-rule-config/models/res_config.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import openerp
+from openerp import api, fields, models, _
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+    
+    module_account_product_fiscal_classification = fields.Boolean(string='account product fiscal classification',
+        help='* Add a new light concept fiscal_classification to associate possible\n'
+              'purchase and sale taxes;\n'
+             '-This installs the module account_product_fiscal_classification.')
+    
+    module_account_product_fiscal_classification_test = fields.Boolean(string='account product fiscal classification test',
+         help=' This module is a technical module to test some features of the module \n' 
+              ' ``account_product_fiscal_classification`` (same repository), that requires \n' 
+              ' chart Template. \n' 
+             '-This installs the module account_product_fiscal_classification_test.')
+    
